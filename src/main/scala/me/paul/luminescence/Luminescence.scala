762411a9235ec761a490aeee418dc75b6be05ebf
Paul Wrubel <pjwrubel@mtu.edu>
2018-11-03 15:46:08 -0400

    basic image file processing added

---
+++ src/main/scala/me/paul/luminescence/Luminescence.scala
@@ -0,0 +1,45 @@
+package me.paul.luminescence
+
+import LoopUtils._
+
+import java.io.{File, IOException}
+
+import javafx.embed.swing.SwingFXUtils
+import javafx.scene.image.WritableImage
+import javax.imageio.ImageIO
+
+object Luminescence {
+
+    def main(args: Array[String]): Unit = {
+        val image = getImage
+
+        // image filling occurs here
+
+        writeImageToFile(image, getFile)
+    }
+
+    def getImage: WritableImage = new WritableImage(Parameters.IMAGE_WIDTH, Parameters.IMAGE_HEIGHT)
+
+    def getFile: File = {
+        val dir = Parameters.FILEPATH
+        if ( !dir.exists && !dir.mkdirs() ) {
+            throw new IOException("[ERROR]: COULD NOT CREATE NECESSARY DIRECTORIES")
+        }
+
+        var count = 0
+        val exists = (f: File) => f.exists()
+        doWhileYield[File](exists) {
+            count += 1
+            new File(Parameters.FILEPATH.getPath + "/" + Parameters.FILENAME + "_" + count + "." + Parameters.IMAGE_FILE_FORMAT)
+        }
+    }
+
+    def writeImageToFile(image: WritableImage, file: File): Unit = {
+        try {
+            ImageIO.write(SwingFXUtils.fromFXImage(image, null), Parameters.IMAGE_FILE_FORMAT, file)
+        } catch {
+            case ioe: IOException =>
+                ioe.printStackTrace()
+        }
+    }
+}
---
+++ src/main/scala/me/paul/luminescence/Parameters.scala
@@ -0,0 +1,16 @@
+package me.paul.luminescence
+
+import java.io.File
+
+object Parameters {
+
+    val VERSION: String = "0.1"
+
+    val IMAGE_WIDTH: Int = 300
+    val IMAGE_HEIGHT: Int = 300
+    val IMAGE_FILE_FORMAT: String = "png"
+
+    val FILENAME: String = s"$VERSION-${IMAGE_WIDTH}x$IMAGE_HEIGHT"
+    val FILEPATH: File = new File(Parameters.getClass.getProtectionDomain.getCodeSource.getLocation.getFile)
+
+}
