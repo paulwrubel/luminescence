0ec6426052c22775d701d5c65ad6afbb3c350f41
Paul Wrubel <pjwrubel@mtu.edu>
2018-11-04 20:44:59 -0500

    incredibly basic ray tracing implementation

--- src/main/scala/me/paul/luminescence/Luminescence.scala
+++ src/main/scala/me/paul/luminescence/Luminescence.scala
@@ -1,17 +1,56 @@
 package me.paul.luminescence
 
-import LoopUtil._
-
 import java.io.{File, IOException}
 
 import javafx.embed.swing.SwingFXUtils
 import javafx.scene.image.WritableImage
+import javafx.scene.paint.Color
 import javax.imageio.ImageIO
+import me.paul.luminescence.LoopUtil._
+import me.paul.luminescence.geometry.{Point3D, Ray3D, Sphere, Vector3D}
 
 object Luminescence {
 
     def main(args: Array[String]): Unit = {
         val image = getImage
+
+        val pixelWriter = image.getPixelWriter
+
+        val viewportCenterLocation = Point3D(0, 0, 0)
+
+        val eyeLocation = viewportCenterLocation + Vector3D(0, 0, 5)
+
+        val nw = viewportCenterLocation + Vector3D(-3, 1.5, 0)
+        val se = viewportCenterLocation + Vector3D(3, -1.5, 0)
+
+        val planeWidth: Double = (nw to se).x.abs
+        val planeHeight: Double = (nw to se).y.abs
+
+        val ball = Sphere(viewportCenterLocation + Vector3D(0, 0, -10), 1, Color.BLACK)
+
+        (0 until Parameters.IMAGE_WIDTH).foreach(x => {
+            ((Parameters.IMAGE_HEIGHT - 1) to 0 by -1).foreach(y => {
+                println("PING: " + ((Parameters.IMAGE_HEIGHT - 1) - y))
+                val planeLocation =
+                    nw + (Vector3D.RIGHT * x * planeWidth / Parameters.IMAGE_WIDTH) + (-Vector3D.UP * y * planeHeight / Parameters.IMAGE_HEIGHT)
+                val rayIntoScene = Ray3D(eyeLocation, eyeLocation to planeLocation)
+
+                val c: Color =
+                    rayIntoScene intersections ball match {
+                        case Some(i) =>
+                            println("We've intersected!")
+                            ball.color
+                        case None =>
+                            println("We've failed...")
+                            println(s"Pixel = $x, $y")
+                            println(s"Ray = $rayIntoScene")
+                            println(s"planeLocation = $planeLocation")
+                            Color.TRANSPARENT
+                    }
+
+                pixelWriter.setColor(x, y, c)
+            })
+        })
 
         // image filling occurs here
 
--- src/main/scala/me/paul/luminescence/Parameters.scala
+++ src/main/scala/me/paul/luminescence/Parameters.scala
@@ -6,11 +6,11 @@ object Parameters {
 
     val VERSION: String = "0.1"
 
-    val IMAGE_WIDTH: Int = 300
-    val IMAGE_HEIGHT: Int = 300
+    val IMAGE_WIDTH: Int = 50
+    val IMAGE_HEIGHT: Int = 50
     val IMAGE_FILE_FORMAT: String = "png"
 
     val FILENAME: String = s"$VERSION-${IMAGE_WIDTH}x$IMAGE_HEIGHT"
-    val FILEPATH: File = new File(new File(Parameters.getClass.getProtectionDomain.getCodeSource.getLocation.getFile).getParent)
+    val FILEPATH: File = new File(new File(Parameters.getClass.getProtectionDomain.getCodeSource.getLocation.getFile).getParent + "/images/")
 
 }
